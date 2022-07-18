cc3a6b5ae280d846fc499e16be78560da871c8cb
Dumebi Nwanze <davisnwanze7@gmail.com>
2022-07-18 14:06:05 +0300

    removed api keys

--- mileon/lib/exercise_api.dart
+++ mileon/lib/exercise_api.dart
@@ -6,7 +6,7 @@ class ExerciseAPI {
   static const String _url = "https://exercisedb.p.rapidapi.com/exercises";
   static const Map<String, String> _headers = {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': 'adcb79d718msh0af3f9939dd7a24p118029jsn7dc9d92f9126'
+    'X-RapidAPI-Key': 'API-KEY'
   };
 
   Future<List<ExerciseApiModel>?> fetchExercises() async {
